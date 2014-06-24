51e0c6ed257d1b527bdac38ecd29519ed98e5de0
Brady Wied <brady@wied.us>
2014-06-24 16:01:33 -0500

    Make it easier to remember how to do this

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.2.1'
 
 set :application, 'rails_test'
 set :repo_url, 'git@github.com:wied03/rails_test.git'
+set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/my_app'
