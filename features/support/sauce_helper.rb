11da443e365cbe85c4ec5a6fd9e635359c179684
Brady Wied <brady@wied.us>
2014-10-03 17:27:44 -0500

    Selective sauce

--- features/support/sauce_helper.rb
+++ features/support/sauce_helper.rb
@@ -1,14 +1,16 @@
 # You should edit this file with the browsers you wish to use
 # For options, check out http://saucelabs.com/docs/platforms
-require 'sauce'
-require "sauce/capybara"
-require 'sauce/cucumber'
+if ENV['LIKEATHESAUCE']
+  require 'sauce'
+  require 'sauce/capybara'
+  require 'sauce/cucumber'
 
-Capybara.default_driver = :sauce
+  Capybara.default_driver = :sauce
 
-Sauce.config do |config|
-  config[:browsers] = [
-      ["Windows 8", "Internet Explorer", "10"]
-  ]
-  config[:start_tunnel] = true
+  Sauce.config do |config|
+    config[:browsers] = [
+        ["Windows 8", "Internet Explorer", "10"]
+    ]
+    config[:start_tunnel] = true
+  end
 end
